b622534413d9253a7d83817076a5aa3101df4dc5
Serhii-Kvach <chilim452@gmail.com>
2025-05-11 05:53:29 +0200

    edit6 hw2-nodejs

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import { getAllContacts, getContactById } from './services/contacts.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
-export const setupServer = () => {
+export const setupServer = async () => {
   const app = express();
 
   app.use(express.json());
@@ -25,12 +25,16 @@ export const setupServer = () => {
   });
 
   app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
+    try {
+      const contacts = await getAllContacts();
+      res.status(200).json({
+        status: 200,
+        message: 'Successfully found contacts!',
+        data: contacts,
+      });
+    } catch (error) {
+      console.error(error);
+    }
   });
 
   app.get('/contacts/:contactId', async (req, res, next) => {
@@ -39,10 +43,7 @@ export const setupServer = () => {
       const contact = await getContactById(contactId);
 
       if (!contact) {
-        res.status(404).json({
-          status: 404,
-          message: `Contact with id ${contactId} not found`,
-        });
+        res.status(404).json({ message: 'Contact not found' });
         return;
       }
 
@@ -53,14 +54,10 @@ export const setupServer = () => {
       });
     } catch (error) {
       console.error(error);
-      res.status(500).json({
-        message: 'Internal Server Error',
-        error: error.message,
-      });
     }
   });
 
-  app.use((req, res) => {
+  app.use((req, res, next) => {
     res.status(404).json({
       message: 'Requested resource could not be found',
     });
