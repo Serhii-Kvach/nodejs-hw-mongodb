e547d706c854877b872570d60a0cde2000b40323
Serhii-Kvach <chilim452@gmail.com>
2025-05-11 05:40:58 +0200

    edit5 hw2-nodejs

--- src/server.js
+++ src/server.js
@@ -33,32 +33,31 @@ export const setupServer = () => {
     });
   });
 
-  app.get('/contacts/:contactId', async (req, res) => {
-    app.get('/contacts/:contactId', async (req, res) => {
-      try {
-        const { contactId } = req.params;
-        const contact = await getContactById(contactId);
+  app.get('/contacts/:contactId', async (req, res, next) => {
+    try {
+      const { contactId } = req.params;
+      const contact = await getContactById(contactId);
 
-        if (!contact) {
-          return res.status(404).json({
-            status: 404,
-            message: `Contact not found`,
-          });
-        }
-
-        res.status(200).json({
-          status: 200,
-          message: `Successfully found contact with id ${contactId}!`,
-          data: contact,
-        });
-      } catch (error) {
-        console.error('/contacts/:contactId:', error);
-        return res.status(404).json({
+      if (!contact) {
+        res.status(404).json({
           status: 404,
-          message: 'Contact not found',
+          message: `Contact with id ${contactId} not found`,
         });
+        return;
       }
-    });
+
+      res.status(200).json({
+        status: 200,
+        message: `Successfully found contact with id ${contactId}!`,
+        data: contact,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        message: 'Internal Server Error',
+        error: error.message,
+      });
+    }
   });
 
   app.use((req, res) => {
