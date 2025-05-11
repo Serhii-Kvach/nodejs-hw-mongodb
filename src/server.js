5c8462968d9b6c4dbd13ce59918e9d27dc58802e
Serhii-Kvach <chilim452@gmail.com>
2025-05-11 05:26:02 +0200

    edit3 hw2-nodejs

--- src/server.js
+++ src/server.js
@@ -38,11 +38,10 @@ export const setupServer = () => {
     const contact = await getContactById(contactId);
 
     if (!contact) {
-      res.status(404).json({
+      return res.status(404).json({
         status: 404,
         message: `Contact not found`,
       });
-      return;
     }
 
     res.status(200).json({
